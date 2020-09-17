07f517fe3635d052e2eff5db2b70939decce4d90
GermanSimba <germansimbasan@gmail.com>
2020-09-17 14:09:42 -0500

    pruebas

--- src/app/services/developers.services.ts
+++ src/app/services/developers.services.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from  '@angular/common/http' 
 import { Developer } from '../models/developer.model';
 const urlContexto= 'https://developersavalithapi.herokuapp.com/developers';
-//const urlContexto= 'http://localhost:3000/developers';
+// const urlContexto= 'http://localhost:3000/developers';
 
 @Injectable({
     providedIn: 'root'
