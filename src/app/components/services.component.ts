ea5395b4eb992725a4dafeab3c37ce155dce051c
GermanSimba <germansimbasan@gmail.com>
2020-09-17 11:21:35 -0500

    funcion actualizar

--- src/app/components/services.component.ts
+++ src/app/components/services.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import {MatTableDataSource} from '@angular/material/table';
-import {DevelopersServices } from '../services/developers.services';
+import { MatTableDataSource } from '@angular/material/table';
+import { DevelopersServices } from '../services/developers.services';
 import { Developer } from '../models/developer.model';
 import { ModalDirective } from 'ngx-bootstrap/modal';
 
@@ -10,59 +10,77 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent  implements OnInit{
+export class ServicesComponent implements OnInit {
   //ELEMENT_DATA :  Developer[]=[];
   //displayedColumns: string[] = ['nombres_completos','tecnologias_conocidas', 'link_gitHub'];
   //data = new MatTableDataSource<Developer>(this.ELEMENT_DATA);
-  respuesta :any;
-  data:Developer[];
-  model :Developer= new Developer();
- 
-  constructor(private ds:DevelopersServices ) { }
+  respuesta: any;
+  data: Developer[];
+  model: Developer = new Developer();
+
+  constructor(private ds: DevelopersServices) { }
 
   ngOnInit() {
     this.getDevelopers();
   }
-  public getDevelopers(){
-    this.ds.getDevelopers().subscribe( res => {
-      //this.data.data=res as  Developer[];
+  public getDevelopers() {
+    this.ds.getDevelopers().subscribe(res => {
       this.respuesta = res;
       this.data = this.respuesta;
       console.info(res)
-     })
+    })
   }
 
-  public delete(develop:Developer){
-    this.ds.delete(develop).subscribe( res => {
-      //this.data.data=res as  Developer[];
+  public delete(develop: Developer) {
+    this.ds.delete(develop).subscribe(res => {
       this.respuesta = res;
       this.data = this.respuesta;
       console.info(res)
-     })
+    })
   }
 
-  showModal(closeSession: ModalDirective){
+  onSubmit(closeSession: ModalDirective) {
+    closeSession.hide();
+    if (this.model.id === 0) {
+      this.createNewDeveloper();
+    } else {
+      this.updateDeveloper();
+    }
+
+  }
+
+  createNewDeveloper() {
+    this.ds.agregarDeveloper(this.model).subscribe(res => {
+      this.respuesta = res;
+      this.data = this.respuesta;
+      this.model = new Developer();
+    })
+  }
+
+  chooseDeveloper(developer: Developer, closeSession: ModalDirective) {
+    this.model = developer;
     closeSession.show();
   }
- 
-  aceptarCerrarSession(closeSession: ModalDirective){
-      localStorage.clear();
-      closeSession.hide();
+
+  updateDeveloper() {
+    this.ds.updateDeveloper(this.model).subscribe(res => {
+      this.respuesta = res;
+      this.data = this.respuesta;
+      this.model = new Developer();
+    })
   }
 
-  cancelarCerrarSession(closeSession: ModalDirective){
+  showModal(closeSession: ModalDirective) {
+    closeSession.show();
+  }
+
+  aceptarCerrarSession(closeSession: ModalDirective) {
+    localStorage.clear();
     closeSession.hide();
   }
 
-  newDeveloper() {
-  }
-
-  onSubmit(){
-      console.info(this.model);
-      this.ds.agregarDeveloper(this.model).subscribe( res => {
-        this.respuesta = res;
-        this.data = this.respuesta;
-       })
+  cancelarCerrarSession(closeSession: ModalDirective) {
+    closeSession.hide();
   }
 
 }
--- src/app/services/developers.services.ts
+++ src/app/services/developers.services.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from  '@angular/common/http' 
 import { Developer } from '../models/developer.model';
 const urlContexto= 'https://developersavalithapi.herokuapp.com/developers';
+//const urlContexto= 'http://localhost:3000/developers';
 
 @Injectable({
     providedIn: 'root'
@@ -23,6 +24,10 @@ export class DevelopersServices{
 
     agregarDeveloper(develop){
         return this.http.post(urlContexto,develop);
+    }
+
+    updateDeveloper(develop){
+        return this.http.put(urlContexto,develop);
     }
 
 }
